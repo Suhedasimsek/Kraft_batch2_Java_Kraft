1d5342087fff0bebfe981d247e06bbb92982b124
isikharun <isikharun@outlook.com>
2022-09-28 00:33:37 +0300

    Exception cont. düzeltme 2

--- src/day_47_Exception_Cont/ExampleTryCatch.java
+++ src/day_47_Exception_Cont/ExampleTryCatch.java
@@ -80,15 +80,40 @@ boolean devam=true;
         //array içerinde en büyük sayının index numarasını getiren metodu yazınız
         //eğer en büyük sayıdan 2 veya daha fazla var ise new exception throw edin.
 
-int[] array={1,4,4,3,1};
+int[] array={1,5,4,3,1};
         System.out.println(maxNumberIndex(array));
     }
     //1,2 ,5,4,5   1,2,4,5,5
     public static int maxNumberIndex(int[] array) throws Exception {
-        int[] arr=new int[array.length];
-     for (int i=0;i<array.length; i++){
-         arr[i]=array[i];
-     }
+
+        /*
+        Bir Array'i farklı bir arrayin içerisine 5 farklı yöntem ile kopyalayabiliriz..
+        1-For loop
+                         int[] arr=new int[array.length];
+                        for (int i=0;i<array.length; i++){
+                         arr[i]=array[i];
+                        }
+
+        2-Arrays.copyOf(int[] original, int newLength)
+
+                         int[] arr=Arrays.copyOf(array,array.length);
+
+        3-Arrays.copyOfRange(int[] original, int from, int to)
+
+                         int[] arr=Arrays.copyOfRange(array,0,array.length-1);
+
+         4-clone()
+
+                        int[] arr=array.clone();
+
+         5-System.arraycopy(Object src, int srcPos, Object dest, int destPos, int length)
+
+                         int[] arr=new int[array.length];
+                         System.arraycopy(array,0,arr,0,array.length);
+
+         */
+
+        int[] arr=array.clone();
 
         int indexNumber=0;
 
