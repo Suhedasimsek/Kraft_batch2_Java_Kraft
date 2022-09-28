ea003b897251d45e58817c0c5ef1004e65a5a85e
isikharun <isikharun@outlook.com>
2022-09-28 23:24:46 +0300

    collection

---
+++ src/day_48_Collections/ArrayList_Example.java
@@ -0,0 +1,33 @@
+package day_48_Collections;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayList_Example {
+
+    public static void main(String[] args) {
+
+
+        ArrayList<String> arrayList=new ArrayList<>();
+
+        arrayList.add("ahmet");
+        arrayList.add("mehmet");
+        arrayList.add("ayşe");
+        arrayList.add("ahmet");
+
+        ArrayList<String> arrayList2=new ArrayList<>();
+        arrayList2.add("ahmet");
+        arrayList2.add("mehmet");
+
+//ahmet mehmet ayşe ahmet
+        System.out.println("ilk durum:"+arrayList.size());
+        System.out.println(arrayList.indexOf("ayşe"));
+        System.out.println("son durum durum:"+arrayList.size());
+        //System.out.println(arrayList.get(0));
+
+
+
+
+
+    }
+}
---
+++ src/day_48_Collections/LinkedList_Example.java
@@ -0,0 +1,41 @@
+package day_48_Collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class LinkedList_Example {
+
+    public static void main(String[] args) {
+        /*
+        ArrayList<String> arrayList=new ArrayList<>();
+        LinkedList<String> linkedList=new LinkedList<>();
+        linkedList.add("ahmet");
+        linkedList.add("mehmet");
+        linkedList.add("ayşe");
+        linkedList.add("Alpaslan");
+
+        System.out.println(linkedList.size());
+        System.out.println(linkedList.pop());
+        System.out.println(linkedList.size());
+        System.out.println(linkedList.get(2)); */
+
+        Vector<String> vector=new Vector<>();
+      //  System.out.println(vector.size());
+        vector.add("ahmet");
+        vector.add("mehmet");
+        vector.add("ayşe");
+        vector.add("Alpaslan");
+        vector.add("ahmet");
+        vector.add("mehmet");
+        vector.add("ayşe");
+        vector.add("Alpaslan");
+        vector.add("ahmet");
+        vector.add("mehmet");
+        vector.add("ayşe");
+        vector.add("Alpaslan");
+        System.out.println(vector.size());
+
+
+    }
+}
